8500e90e48ed3290947ed43394337fe5acd4632e
fermi_dirac <ManueleBryan@gmail.com>
2018-04-04 17:01:10 -0700

    app complete

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      diceValue: undefined,
+    }
+
+    axios.get('/api/random/dice')
+      .then((response) => {
+        return response.data.value;
+      })
+      .then((diceValue) => {
+        this.setState({diceValue})
+      })
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        Random Dice Roll: {this.state.diceValue}
       </div>
     );
   }
